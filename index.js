e720f048d2d1072a8346cc2da04c9e5b86956861
adesh <adesh@silverbirchstudios.com>
2023-11-29 20:15:30 +0530

    not working

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
+import React from 'react';
 import App from './App';
 import {name as appName} from './app.json';
 
+const MyHeadlessTask = async () => {
+  console.log('Receiving HeartBeat!');
+};
+
+AppRegistry.registerHeadlessTask('Heartbeat', () => MyHeadlessTask);
 AppRegistry.registerComponent(appName, () => App);
