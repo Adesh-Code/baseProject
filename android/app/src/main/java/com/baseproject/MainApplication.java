72acd24390d08b52386c355c263a575318e1bf53
adesh <adesh@silverbirchstudios.com>
2023-11-29 21:01:36 +0530

    not working

--- android/app/src/main/java/com/baseproject/MainApplication.java
+++ android/app/src/main/java/com/baseproject/MainApplication.java
@@ -9,6 +9,8 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import java.util.Array;
+import com.baseproject.BackgroundTimerPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -21,11 +23,9 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
+          return Arrays.<ReactPackage>asList(
+              new BackgroundTimerPackage()
+          );
         }
 
         @Override
